ac0b3ab68ab9a58eb9edacf3a6ee57693fddc5c3
Franklin Liu <fezhead@parabolas.xyz>
2023-05-06 20:59:08 -0700

    add search functionality to SearchForm.jsx

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,28 +1,58 @@
+import { Box, VStack, SimpleGrid, Card, CardBody, Image, Text, CardFooter, ButtonGroup, Input, Stack, Button } from '@chakra-ui/react';
 import { useState } from 'react';
+import axios from 'axios';
+
+function SearchResult({ videoData }) {
+  return (
+    <Card>
+      <CardBody>
+        <Image src={videoData.thumbnail} />
+        <Text>{videoData.title}</Text>
+      </CardBody>
+      <CardFooter>
+        <ButtonGroup spacing={2}>
+        </ButtonGroup>
+      </CardFooter>
+    </Card>
+  )
+}
+
 
 const SearchForm = () => {
   const [searchTerm, setSearchTerm] = useState();
+  const [APIData, setAPIData] = useState([]);
   const handleSubmit = (e) => {
     e.preventDefault();
     const searchValue = e.target.elements.search.value;
     if (!searchValue) return;
     setSearchTerm(searchValue);
+    axios.get("https://pipedapi.kavin.rocks/search", { params: { "q": searchValue, "filter": "music_songs" } }).then((response) => { setAPIData(response.data.items) })
   };
   return (
-    <section>
-        <h1>Dancypants</h1>
-      <form className='search-form' onSubmit={handleSubmit}>
-        <input
-          type='text'
-          className='form-input search-input'
-          name='search'
-          placeholder='Search'
-        />
-        <button type='submit' className='btn'>
-          search
-        </button>
-      </form>
-    </section>
+    <Box>
+      <h1>Dancypants</h1>
+      <VStack spacing={8}>
+        <form className='search-form' onSubmit={handleSubmit}>
+          <Stack direction='row'>
+            <Input
+              type='text'
+              // className='form-input search-input'
+              name='search'
+              placeholder='Search for a song'
+            />
+            <Button type='submit'>
+              Search
+            </Button>
+          </Stack>
+        </form>
+        <SimpleGrid columns={5} spacing={7}>
+          {
+            APIData.map(video =>
+              (<SearchResult videoData={video} key={video.url.substring(9)} />))
+          }
+        </SimpleGrid>
+      </VStack>
+    </Box>
   );
 };
 export default SearchForm;
