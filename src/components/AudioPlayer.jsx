8a0a7b0e65e774abe20b2cd39435733ae1a2d8ea
Franklin Liu <fezhead@parabolas.xyz>
2023-05-11 22:44:52 -0700

    draw subtitles on canvas

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -1,6 +1,34 @@
+import { useEffect, useRef } from 'react';
+import useEventListener from '../hooks/useEventListener'
+
 function AudioPlayer() {
+  const canvasRef = useRef(null)
+  const trackRef = useRef(null)
+  useEventListener(trackRef, "cuechange", (event) => {
+    const canvas = canvasRef.current
+    const context = canvas.getContext('2d')
+    let cues = event.target.track.activeCues[0].text;
+    console.log(cues)
+    console.log("meow")
+    context.font = "40px sans serif"
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    context.fillText(cues, canvas.width / 2, canvas.height / 2)
+  })
+
+  let trackPlayer =
+    <track default
+      ref={trackRef} kind="captions"
+      srcLang="zh"
+      src="/yijianmei.vtt" />
+  let lyricDisplay = <canvas ref={canvasRef} width={700} ></canvas >
+
   return (
-    <audio controls src="https://vid.puffyan.us/latest_version?id=W8x4m-qpmJ8"></audio>
+    <div>
+      {lyricDisplay}
+      <audio controls src="/vocals.wav">
+        {trackPlayer}
+      </audio>
+    </div>
   )
 }
 
