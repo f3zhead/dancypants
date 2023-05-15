e544ce36a364a9d5078978661f21eeffaa53abdf
wenjia01 <mars200814@gmail.com>
2023-05-14 20:22:34 -0700

    centered the breadcrumb

--- src/components/Navigationbar.jsx
+++ src/components/Navigationbar.jsx
@@ -1,16 +1,18 @@
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, Image, Stack} from '@chakra-ui/react'
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, Image, Stack, Center} from '@chakra-ui/react'
 import Logo from '../assets/logo.svg'
 
 const Navigationbar = () => {
   return (
-    <Breadcrumb fontWeight='medium' fontSize='6xl'>
-      <BreadcrumbItem isCurrentPage>
-        <Stack direction='row'>
-          <Image src={Logo} alt='Dan Abramov' boxSize='100px'/>
-          <BreadcrumbLink href='/'>Dancypants</BreadcrumbLink>
-        </Stack>
-      </BreadcrumbItem>
-    </Breadcrumb>
+    <Center>
+      <Breadcrumb fontWeight='medium' fontSize='6xl'>
+        <BreadcrumbItem isCurrentPage>
+          <Stack direction='row'>
+            <Image src={Logo} alt='Dan Abramov' boxSize='100px'/>
+            <BreadcrumbLink href='/'>Dancypants</BreadcrumbLink>
+          </Stack>
+        </BreadcrumbItem>
+      </Breadcrumb>
+    </Center>
   );
 };
 
